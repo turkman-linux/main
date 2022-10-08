db8f73e7fcb077bbcde5214e4dfcfcb6daa96376
sulincix <parduscix@yandex.ru>
2022-10-08 13:35:16 +0000

    beter index script output

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 find -type f | grep ympbuild | while read line ; do
+    echo -e "\033[33;1m=>> SOURCE BUILD START:\033[;0m $line"
     ymp build $(dirname $line) --verbose --no-binary --ignore-dependency --allow-oem
+        echo -e "\033[33;1m<<= SOURCE BUILD DONE:\033[;0m $line"
 done
 [[ -d output ]] && rm -rf output
 mkdir -p output
