7297ae784220aee7555e126d9d2646baec75cbae
sulincix <parduscix@yandex.ru>
2022-10-08 13:05:37 +0000

    html index

--- update.sh
+++ update.sh
@@ -4,7 +4,9 @@ find -type f | grep ympbuild | while read line ; do
 done
 [[ -d output ]] && rm -rf output
 mkdir -p output
-find -type f | grep "source.ymp$" | while read line ; do
+find -type f | grep "source.ymp$" | sort -V | while read line ; do
     mv $line output
+    echo "* <a href=\"$(basename $line)\">$(basename $line)</a><br>" >> output/index.html
 done
 ymp index output --allow-oem
+echo "* <a href=\"ymp-index.yaml\">ymp-index.yaml</a><br>" >> output/index.html >> output/index.html
