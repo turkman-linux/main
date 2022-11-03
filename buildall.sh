e243e40183e416ce0fb416c9c2435903e4c319eb
aliriza <aliriza.keskin@pardus.org.tr>
2022-11-03 07:24:08 +0000

    buildall

---
+++ buildall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+find -type f | grep ympbuild | while read line ; do
+    echo -e "\033[33;1m=>> BINARY BUILD START:\033[;0m $line"
+    ymp build $(dirname $line) --verbose --no-source --ignore-dependency --allow-oem
+        echo -e "\033[33;1m<<= BINARY BUILD DONE:\033[;0m $line"
+done
+[[ -d output ]] && rm -rf output
+mkdir -p output
+echo "<h1><b>YMP index:</b></h1><br>" > output/index.html
+find -type f | grep ".ymp$" | sort -V | while read line ; do
+    mv $line output
+    echo "* <a href=\"$(basename $line)\">$(basename $line)</a><br>" >> output/index.html
+done
+ymp index output --allow-oem
+echo "* <a href=\"ymp-index.yaml\">ymp-index.yaml</a><br>" >> output/index.html
