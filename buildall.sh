68acb252d0048893387d95ac2badeef03f24e4fd
aliriza <aliriza.keskin@pardus.org.tr>
2022-11-07 12:14:24 +0000

    update scripts

--- buildall.sh
+++ buildall.sh
@@ -2,13 +2,15 @@
 set -e
 find -type f | grep ympbuild | while read line ; do
     echo -e "\033[33;1m=>> BINARY BUILD START:\033[;0m $line"
-    ymp build $(dirname $line) --verbose --no-source --ignore-dependency --allow-oem
-        echo -e "\033[33;1m<<= BINARY BUILD DONE:\033[;0m $line"
+    if ! ls $(dirname $line)/*$(uname -m).ymp ; then
+        ymp build $(dirname $line) --verbose --no-source --ignore-dependency --allow-oem
+    fi
+    echo -e "\033[33;1m<<= BINARY BUILD DONE:\033[;0m $line"
 done
 [[ -d output ]] && rm -rf output
 mkdir -p output
 echo "<h1><b>YMP index:</b></h1><br>" > output/index.html
-find -type f | grep ".ymp$" | sort -V | while read line ; do
+find -type f | grep "$(uname -m).ymp$" | sort -V | while read line ; do
     mv $line output
     echo "* <a href=\"$(basename $line)\">$(basename $line)</a><br>" >> output/index.html
 done
--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 find -type f | grep ympbuild | while read line ; do
     echo -e "\033[33;1m=>> SOURCE BUILD START:\033[;0m $line"
-    ymp build $(dirname $line) --verbose --no-binary --ignore-dependency --allow-oem
+    if ! ls $(dirname $line)/*.source.ymp ; then
+        ymp build $(dirname $line) --verbose --no-binary --ignore-dependency --allow-oem
         echo -e "\033[33;1m<<= SOURCE BUILD DONE:\033[;0m $line"
+    fi
 done
 [[ -d output ]] && rm -rf output
 mkdir -p output
