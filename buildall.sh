0b940b76f6f69cc5521d4a6b829146e2b22ba44d
sulincix <parduscix@yandex.ru>
2022-11-09 21:32:22 +0000

    script update

--- buildall.sh
+++ buildall.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
+move_package(){
+    mkdir -p output
+    find -type f  | grep -v "output"| grep "$(uname -m).ymp$" | sort -V | while read line ; do
+        mv $line output
+    done
+}
+
 find -type f | grep ympbuild | sort -V | while read line ; do
     name=$(basename $(dirname $line))
     path=$(realpath $(dirname $line))
     if ! find ./output/ -type f | grep "${name}_" | grep "$(uname -m).ymp$" 2>/dev/null ; then
         echo -e "\033[33;1m<<= BINARY BUILD DONE:\033[;0m $line"
         ymp --sandbox --shared="$path" build "$path" --verbose --no-source --ignore-dependency --allow-oem
+        move_package
         echo -e "\033[33;1m<<= BINARY BUILD DONE:\033[;0m $line"
     fi
 done
-mkdir -p output
-find -type f  | grep -v "output"| grep "$(uname -m).ymp$" | sort -V | while read line ; do
-    mv $line output
-done
+
 ymp index output --allow-oem --verbose --move
--- update.sh
+++ update.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
+move_source(){
+    mkdir -p output
+    find -type f  | grep -v "output"| grep "source.ymp$" | sort -V | while read line ; do
+        mv $line output
+    done
+}
 find -type f | grep ympbuild | sort -V | while read line ; do
     name=$(basename $(dirname $line))
     path=$(realpath $(dirname $line))
     if ! find ./output/ -type f | grep "${name}_" | grep "source.ymp$" 2>/dev/null ; then
         echo -e "\033[33;1m=>> SOURCE BUILD START:\033[;0m $line"
         ymp --sandbox --shared="$path" build "$path" --verbose --no-binary --ignore-dependency --allow-oem
+        move_source
         echo -e "\033[33;1m<<= SOURCE BUILD DONE:\033[;0m $line"
     fi
-done
-mkdir -p output
-find -type f  | grep -v "output"| grep "source.ymp$" | sort -V | while read line ; do
-    mv $line output
 done
 ymp index output --allow-oem --verbose --move
 
