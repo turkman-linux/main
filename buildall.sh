91452c8ab7ae83a83b64a53b19f3baf9ff5b6331
sulincix <parduscix@yandex.ru>
2022-12-07 17:46:59 +0000

    nano

--- buildall.sh
+++ buildall.sh
@@ -11,7 +11,7 @@ find -type f | grep ympbuild | sort -V | while read line ; do
     path=$(realpath $(dirname $line))
     if ! find ./output/ -type f | grep "${name}/${name}_" | grep "$(uname -m).ymp$" &>/dev/null ; then
         echo -e "\033[33;1m<<= BINARY BUILD DONE:\033[;0m $line"
-        ymp --sandbox --shared="$path" build "$path" --no-source --ignore-dependency --use=all --allow-oem || exit 1
+        ymp --sandbox --shared="$path" build "$path" --no-source --ignore-dependency --use=all --allow-oem --ignore-ssl || exit 1
         move_package
         echo -e "\033[33;1m<<= BINARY BUILD DONE:\033[;0m $line"
     fi
