35dec2e5c88e03d7b82b8230fbd233d5c6c50506
sulincix <parduscix@yandex.ru>
2022-12-13 17:48:58 +0000

    fix script

--- update.sh
+++ update.sh
@@ -2,16 +2,16 @@
 set -e
 move_source(){
     mkdir -p output
-    find -type f  | grep -v "output"| grep "source.ymp$" | sort -V | while read line ; do
+    find -type f  | grep -v "output"| grep ".ymp$" | sort -V | while read line ; do
         mv $line output
     done
 }
 find -type f | grep ympbuild | sort -V | while read line ; do
     name=$(basename $(dirname $line))
     path=$(realpath $(dirname $line))
-    if ! find ./output/ -type f | grep "${name}/${name}_" | grep "source.ymp$" &>/dev/null ; then
+    if ! find ./output/ -type f | grep "${name}/${name}_" | grep ".ymp$" &>/dev/null ; then
         echo -e "\033[33;1m=>> SOURCE BUILD START:\033[;0m $line"
-        ymp --sandbox --shared="$path" build "$path" --ignore-dependency --use=all --allow-oem --verbose || exit 1
+        ymp --sandbox --shared="$path" build "$path" --ignore-dependency --use=all --allow-oem $@ --verbose || exit 1
         move_source
         echo -e "\033[33;1m<<= SOURCE BUILD DONE:\033[;0m $line"
     fi
