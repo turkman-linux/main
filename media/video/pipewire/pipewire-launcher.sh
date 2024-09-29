226c90fca7bc9589d779e117fb8f4ccfe66419ea
sulincix <parduscix@yandex.ru>
2024-09-29 19:56:12 +0000

    replace wireplumber as user daemon

--- media/video/pipewire/pipewire-launcher.sh
+++ media/video/pipewire/pipewire-launcher.sh
@@ -6,22 +6,21 @@ umask 002
 
 # We need to kill any existing pipewire instance to restore sound
 pkill -u "${USER}" -fx /usr/bin/pipewire-pulse 1>/dev/null 2>&1
-pkill -u "${USER}" -fx /usr/bin/pipewire-media-session 1>/dev/null 2>&1
-pkill -u "${USER}" -fx /usr/bin/wireplumber 1>/dev/null 2>&1
 pkill -u "${USER}" -fx /usr/bin/pipewire 1>/dev/null 2>&1
 
-exec /usr/bin/pipewire &
+dbus=''
+if [ -f /usr/bin/dbus-run-session ] ; then
+    dbus='/usr/bin/dbus-run-session'
+fi
+
+
+exec $dbus /usr/bin/pipewire &
 
 # wait for pipewire to start before attempting to start related daemons
 while [ "$(pgrep -f /usr/bin/pipewire)" = "" ]; do
         sleep 1
 done
 
-if [ -x /usr/bin/wireplumber ]; then
-	exec /usr/bin/wireplumber &
-elif [ -x /usr/bin/pipewire-media-session ]; then
-	exec /usr/bin/pipewire-media-session &
-fi
 # wait until start pipewire
 while ! [ -e /run/pipewire/pipewire-0 ] ; do
     sleep 1
@@ -29,11 +28,7 @@ done
 chgrp audio -R /run/pipewire/
 # start pipewire-pulse
 if [ -f "/usr/share/pipewire/pipewire-pulse.conf" ] ; then
-    dbus=''
-    if [ -f /usr/bin/dbus-run-session ] ; then
-        dbus='/usr/bin/dbus-run-session'
-    fi
-    exec $dus /usr/bin/pipewire-pulse &
+    exec $dbus /usr/bin/pipewire-pulse &
     # wait until start pipewire-pulse
     while ! [ -e /run/pipewire/pulse/native ] ; do
         sleep 1
