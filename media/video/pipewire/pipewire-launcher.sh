597cf65d797483ace9d4c42a93a26c2e0dbdf424
sulincix <parduscix@yandex.ru>
2024-09-29 18:51:09 +0000

    run pipewire as system service & remove libpulse daemons

--- media/video/pipewire/pipewire-launcher.sh
+++ media/video/pipewire/pipewire-launcher.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
-
-# we must exit if cinnamon desktop detected
-if [ "$XDG_CURRENT_DESKTOP" == "X-Cinnamon" ]; then
-    exit 0
-fi
+#!/bin/bash
+export PIPEWIRE_RUNTIME_DIR=/run/pipewire
+export XDG_RUNTIME_DIR=/run/pipewire
+export PULSE_RUNTIME_PATH=/run/pipewire/pulse
+umask 002
 
 # We need to kill any existing pipewire instance to restore sound
 pkill -u "${USER}" -fx /usr/bin/pipewire-pulse 1>/dev/null 2>&1
@@ -23,5 +22,23 @@ if [ -x /usr/bin/wireplumber ]; then
 elif [ -x /usr/bin/pipewire-media-session ]; then
 	exec /usr/bin/pipewire-media-session &
 fi
-
-[ -f "/usr/share/pipewire/pipewire-pulse.conf" ] && exec /usr/bin/pipewire-pulse &
+# wait until start pipewire
+while ! [ -e /run/pipewire/pipewire-0 ] ; do
+    sleep 1
+done
+chgrp audio -R /run/pipewire/
+# start pipewire-pulse
+if [ -f "/usr/share/pipewire/pipewire-pulse.conf" ] ; then
+    dbus=''
+    if [ -f /usr/bin/dbus-run-session ] ; then
+        dbus='/usr/bin/dbus-run-session'
+    fi
+    exec $dus /usr/bin/pipewire-pulse &
+    # wait until start pipewire-pulse
+    while ! [ -e /run/pipewire/pulse/native ] ; do
+        sleep 1
+    done
+    chgrp audio -R /run/pipewire/pulse
+    chmod 775 /run/pipewire/pulse
+fi
+wait
