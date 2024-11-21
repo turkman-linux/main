ebd0e07bd11392ac51bf1415f3e2d1bfe0d0e27c
aliriza <aliriza.keskin@pardus.org.tr>
2024-11-21 22:25:49 +0300

    pipewire launcher start without /run/user/<uid>/pipewire

--- media/video/pipewire/pipewire-launcher.sh
+++ media/video/pipewire/pipewire-launcher.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export PIPEWIRE_RUNTIME_DIR=$HOME/.config/pipewire
+export XDG_RUNTIME_DIR=$HOME/.config
+
 # We need to kill any existing pipewire instance to restore sound
 pkill -u "${USER}" -fx /usr/bin/pipewire-pulse 1>/dev/null 2>&1
 pkill -u "${USER}" -fx /usr/bin/pipewire-media-session 1>/dev/null 2>&1
@@ -19,4 +22,6 @@ elif [ -x /usr/bin/pipewire-media-session ]; then
 	exec /usr/bin/pipewire-media-session &
 fi
 
-[ -f "/usr/share/pipewire/pipewire-pulse.conf" ] && exec /usr/bin/pipewire-pulse &
+if [ -f "/usr/share/pipewire/pipewire-pulse.conf" ] ; then
+    exec /usr/bin/pipewire-pulse &
+fi
