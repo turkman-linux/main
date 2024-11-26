5893fa2b598f032680d86e80db7a17b282864ca6
sulincix <parduscix@yandex.ru>
2024-11-26 23:38:14 +0000

    lightdm sway login fix

---
+++ x11/misc/lightdm/lightdm-session.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ ${XDG_SESSION_TYPE} == "wayland" ] ; then
+    # Wait until the Xorg process for the user finishes
+    while pgrep -u 0 Xorg > /dev/null; do
+        sleep 0.1
+    done
+    if [ "${XDG_RUNTIME_DIR}" == "" ] ; then
+        export XDG_RUNTIME_DIR=/tmp/runtime-$USER
+        mkdir -p ${XDG_RUNTIME_DIR}
+    fi
+    exec env dbus-run-session $@
+else
+    exec /etc/X11/xinit/Xsession $@
+fi
